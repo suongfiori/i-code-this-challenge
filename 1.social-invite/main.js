1343d7ea1ceef4c24ddafa482f865ae8e07c64d7
suongfiori <suong.kf@gmail.com>
2024-01-06 14:42:44 +0000

    Add a new coding challenge

---
+++ 1.social-invite/main.js
@@ -0,0 +1,53 @@
+import './style.css'
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const checkbox = document.getElementById('checkbox');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const checkboxChecked = checkbox.checked;
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(!checkboxChecked) {
+		setErrorFor(checkbox, 'Please check this box if you want to proceed');
+	} else {
+		setSuccessFor(checkbox);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+
+
+
+
+
+
+
+
+
+
+
